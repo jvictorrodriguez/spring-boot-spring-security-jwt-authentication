2fcaa42a92355785942c9d30add300385b95f770
Víctor Rodríguez <vrodriguezbarbeira@gmail.com>
2023-11-05 10:09:19 +0100

    First commit

--- src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
+++ src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
@@ -96,7 +96,8 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     http.authenticationProvider(authenticationProvider());
 
     http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    
+
+
     return http.build();
   }
 }
